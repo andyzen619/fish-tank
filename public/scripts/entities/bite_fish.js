e43a06b16425e5a9ecd5ff0fa96ec91e88136518
andyzen619 <andyzen619@gmail.com>
2019-10-26 16:47:29 +0000

    Added bite_fish.js

---
+++ public/scripts/entities/bite_fish.js
@@ -0,0 +1,11 @@
+class BiteFish extends Fish {
+
+  constructor(options) {
+    super(options);
+    this.imageUri = '/images/fzYjRZ9.gif';
+  }
+
+  onClick(event) {
+    this.makeNewVelocity(50);
+  }
+}
--- public/scripts/entities/fish.js
+++ public/scripts/entities/fish.js
@@ -34,4 +34,3 @@ class Fish extends Denizen {
   }
 
 }
-
